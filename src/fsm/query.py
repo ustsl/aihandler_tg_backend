d9188b581fbdcb72dc470768d2e1e8a5bb8d1ed9
ustsl <w@imvo.site>
2024-05-22 17:11:47 +0300

    update state

--- src/fsm/query.py
+++ src/fsm/query.py
@@ -2,4 +2,5 @@ from aiogram.fsm.state import StatesGroup, State
 
 
 class QueryState(StatesGroup):
+    wait = State()
     story = State()
--- src/handlers/base.py
+++ src/handlers/base.py
@@ -1,6 +1,6 @@
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
-from aiogram import Router
+from aiogram import F, Router
 
 from src.actions.query.base import post_query
 from src.actions.user.base import get_or_create_user, get_user
@@ -10,6 +10,7 @@ from src.keyboards.keyboard import main as kb
 from src.messages.base import about_message, start_message
 
 from aiogram.fsm.context import FSMContext
+
 
 from src.settings import API_DOMAIN, API_MAIN_TOKEN
 
@@ -35,7 +36,7 @@ async def clear_handler(message: Message, state: FSMContext) -> None:
 @router.message(Command("refresh_token"))
 async def clear_handler(message: Message) -> None:
     url = f"{API_DOMAIN}v1/users/{str(message.from_user.id)}/token"
-    result = await put_request(url=url, headers={"Authorization": API_MAIN_TOKEN})
+    await put_request(url=url, headers={"Authorization": API_MAIN_TOKEN})
     await message.answer("Token was refreshed", reply_markup=kb)
 
 
@@ -47,17 +48,25 @@ async def clear_handler(message: Message, state: FSMContext) -> None:
     await message.answer(msg, reply_markup=kb)
 
 
-@router.message()
+@router.message(QueryState.wait)
+async def wait_handler(message: Message, state: FSMContext) -> None:
+    await message.answer(
+        "Wait. Everything is fine - the request is being processed.", parse_mode=None
+    )
+
+
+@router.message(F.text)
 async def post_query_handler(message: Message, state: FSMContext) -> None:
     try:
+        # SET state process
+        await state.set_state(QueryState.wait)
+
         # Get story data
         data = await state.get_data()
         old_story = []
         if data.get("story"):
             old_story = data.get("story")
 
-        # Base action
-        await state.set_state(QueryState.story)
         user = await get_user(message.from_user.id)
         prompt_id = user.get("settings").get("prompt_id")
         token = user.get("token").get("token")
@@ -82,12 +91,14 @@ async def post_query_handler(message: Message, state: FSMContext) -> None:
                 {"role": "system", "content": result.get("clean")},
             ]
             new_story = [*old_story, *current_story][-30:]  # 30 - limit
+            await state.set_state(QueryState.story)
             await state.update_data(story=new_story)
         if msg:
             await message.answer(msg, reply_markup=kb, parse_mode=None)
         else:
             await message.answer("Error")
     except Exception as e:
+        await state.clear()
         await message.answer(
             "Error. Try clearing your message history using the /clear command and try again."
         )
